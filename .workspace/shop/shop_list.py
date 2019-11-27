bcb7b8cb9ad14d7878988af2cb24c8433a408ab2
TowardtheStars <hty19980402@qq.com>
2019-11-27 10:49:36 +0800

    删除了无用的代码

--- .workspace/shop/shop_list.py
+++ .workspace/shop/shop_list.py
@@ -26,113 +26,114 @@ output.writerows(gen_rows(sell_list_sheet))
 print("Finished")
 file.close()
 
-print("Exporting shop list pdf..")
-
-print("Parsing shop list")
-category_buy_name = []
-category_sell_name = []
-
-buy_list = {}
-sell_list = {}
-
-BUY = True
-SELL = False
-
-class Entry:
-
-    names_loaded = False
-    names_dict = {}
-    icons_path = "./.workspace/icons"
-
-    @staticmethod
-    def load_names():
-        for directory in os.listdir(Entry.icons_path):
-            file = open(os.path.join(Entry.icons_path, directory, "_names.json"), "r", encoding="utf-8")
-            Entry.names_dict.update(json.load(file))
-        Entry.names_loaded = True
-
-    def __init__(self, item:str, price:float, category:int, shop_mode:bool):
-        spl = item.split(":", 1)
-        self._mod_id = spl[0]
-        spl = spl[1].split(" ", 1)
-        if len(spl) < 2:
-            spl.append("")
-        self._item, self._nbt = tuple(spl)
-        self._item = self._item.split(":", 1)
-        self._metadata = 0
-        if len(self._item) == 2:
-            self._metadata = int(self._item[1])
-        self._item = self._item[0]
-        self._price = price
-        if shop_mode == BUY:
-            buy_list.get(category_buy_name[category]).append(self)
-        elif shop_mode == SELL:
-            sell_list.get(category_sell_name[category]).append(self)
-
-    @property
-    def mod_id(self):
-        return self._mod_id or ""
-
-    @property
-    def item_id(self):
-        return self._item or ""
-
-    @property
-    def metadata(self) -> int: 
-        return self._metadata or 0
-
-    @property
-    def unlocalized_name(self):
-        return self.mod_id + ":" + self.item_id + ":%d" % self.metadata
-    
-    @property
-    def pic_path(self):
-        raw =  ".workspace/icons/{modid}/{modid}__{name}__{metadata}.png".format(modid=self.mod_id, name=self.item_id, metadata=self.metadata)
-        return raw
-
-    @property
-    def localized_name(self):
-        if not Entry.names_loaded:
-            Entry.load_names()
-        return Entry.names_dict.get(self.unlocalized_name, "")
-
-    @property
-    def price(self):
-        return self._price or 0.0
-
-    def __repr__(self):
-        raw = "\t\\includegraphics{" + self.pic_path + "} & " 
-        raw2 = self.localized_name + " &%.2f" % self.price + "\\\\\n\t\\hline\n"
-        raw2 = raw2.replace("_", "\\_", -1)
-        return raw + raw2
-        # return raw.replace("_", "\\_", -1)
-
-    def __str__(self):
-            return self.__repr__()
-
-for row in header.get_rows():
-    if row[0].value == "Buy Category Names:":
-        category_buy_name = [str(c.value) for c in row[1:] if str(c.value) != ""]
-    if row[0].value == "Sell Category Names:":
-        category_sell_name = [str(c.value) for c in row[1:] if str(c.value) != ""]
-
-for names in category_buy_name:
-    buy_list[names] = list()
-
-for names in category_sell_name:
-    sell_list[names] = list()
-
-for row in buy_list_sheet.get_rows():
-    if row[0].value == "buy":
-        Entry(row[1].value, float(row[2].value), int(row[3].value), BUY)
-
-for row in sell_list_sheet.get_rows():
-    if row[0].value == "sell":
-        Entry(row[1].value, float(row[2].value), int(row[3].value), SELL)
-
-print("Parse complete.")
-
 def generate_latex():
+        
+    print("Exporting shop list pdf..")
+
+    print("Parsing shop list")
+    category_buy_name = []
+    category_sell_name = []
+
+    buy_list = {}
+    sell_list = {}
+
+    BUY = True
+    SELL = False
+
+    class Entry:
+
+        names_loaded = False
+        names_dict = {}
+        icons_path = "./.workspace/icons"
+
+        @staticmethod
+        def load_names():
+            for directory in os.listdir(Entry.icons_path):
+                file = open(os.path.join(Entry.icons_path, directory, "_names.json"), "r", encoding="utf-8")
+                Entry.names_dict.update(json.load(file))
+            Entry.names_loaded = True
+
+        def __init__(self, item:str, price:float, category:int, shop_mode:bool):
+            spl = item.split(":", 1)
+            self._mod_id = spl[0]
+            spl = spl[1].split(" ", 1)
+            if len(spl) < 2:
+                spl.append("")
+            self._item, self._nbt = tuple(spl)
+            self._item = self._item.split(":", 1)
+            self._metadata = 0
+            if len(self._item) == 2:
+                self._metadata = int(self._item[1])
+            self._item = self._item[0]
+            self._price = price
+            if shop_mode == BUY:
+                buy_list.get(category_buy_name[category]).append(self)
+            elif shop_mode == SELL:
+                sell_list.get(category_sell_name[category]).append(self)
+
+        @property
+        def mod_id(self):
+            return self._mod_id or ""
+
+        @property
+        def item_id(self):
+            return self._item or ""
+
+        @property
+        def metadata(self) -> int: 
+            return self._metadata or 0
+
+        @property
+        def unlocalized_name(self):
+            return self.mod_id + ":" + self.item_id + ":%d" % self.metadata
+        
+        @property
+        def pic_path(self):
+            raw =  ".workspace/icons/{modid}/{modid}__{name}__{metadata}.png".format(modid=self.mod_id, name=self.item_id, metadata=self.metadata)
+            return raw
+
+        @property
+        def localized_name(self):
+            if not Entry.names_loaded:
+                Entry.load_names()
+            return Entry.names_dict.get(self.unlocalized_name, "")
+
+        @property
+        def price(self):
+            return self._price or 0.0
+
+        def __repr__(self):
+            raw = "\t\\includegraphics{" + self.pic_path + "} & " 
+            raw2 = self.localized_name + " &%.2f" % self.price + "\\\\\n\t\\hline\n"
+            raw2 = raw2.replace("_", "\\_", -1)
+            return raw + raw2
+            # return raw.replace("_", "\\_", -1)
+
+        def __str__(self):
+                return self.__repr__()
+
+    for row in header.get_rows():
+        if row[0].value == "Buy Category Names:":
+            category_buy_name = [str(c.value) for c in row[1:] if str(c.value) != ""]
+        if row[0].value == "Sell Category Names:":
+            category_sell_name = [str(c.value) for c in row[1:] if str(c.value) != ""]
+
+    for names in category_buy_name:
+        buy_list[names] = list()
+
+    for names in category_sell_name:
+        sell_list[names] = list()
+
+    for row in buy_list_sheet.get_rows():
+        if row[0].value == "buy":
+            Entry(row[1].value, float(row[2].value), int(row[3].value), BUY)
+
+    for row in sell_list_sheet.get_rows():
+        if row[0].value == "sell":
+            Entry(row[1].value, float(row[2].value), int(row[3].value), SELL)
+
+    print("Parse complete.")
+
     print("Generating LaTeX")
     header_str = \
     """
