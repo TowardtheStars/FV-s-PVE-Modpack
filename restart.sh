89ff9f0e5ecbba69b5bf9fa8ffa1a37007e1744d
TowardtheStars <hty19980402@qq.com>
2019-11-27 11:18:47 +0800

    增加从控制台直接启动的功能

--- restart.sh
+++ restart.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
 echo "请将启动脚本放置于./run.sh中"
-read -p "请输入最小稳定运行时间(单位: 秒): " min_time
-
+if [ $# -eq 0 ]
+then
+    read -p "请输入最小稳定运行时间(单位: 秒): " min_time
+else
+    min_time=$1
+fi
 start_time=0
 end_time=${start_time}+${time}
 
@@ -12,6 +16,6 @@ do
     sh ./run.sh
     end_time=`date +%s`  
 done
-echo "! 程序于最短稳定运行时间内终止"
+echo "! 程序于最短稳定运行时间内(${min_time}s)终止"
 echo "! 停止自动重启"
-echo "! 请查看log/crash report"
+echo "! 请查看log和crash report"
